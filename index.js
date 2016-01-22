c7b98d37cd529c8254a2e3641a108e798cb19927
Matt Huntington <matt.huntington@gmail.com>
2016-01-21 19:53:33 -0500

    read in file from command line, spit it out

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log(process.argv);
+fs = require('fs');
+
+var parseFile = function(data){
+	console.log(data);
+}
+fs.readFile(process.argv[2], 'utf8', function(err, data){
+	if(err){
+		console.log(err);
+	} else {
+		parseFile(data);
+	}
+});
