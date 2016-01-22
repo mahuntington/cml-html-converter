2299d9a40eafa9ba6a426ca5f9b66b62357ebe4c
Matt Huntington <matt.huntington@gmail.com>
2016-01-21 20:32:33 -0500

    can tell when new indentation happens

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 fs = require('fs');
 
-var parseFile = function(data){
-	console.log(data);
+
+var parseFile = function(data, callback){
+	var previous_line = -1;
+	var lines = data.split('\n');
+	var html = '';
+	lines.forEach(function(value, index){
+		if(value !== ''){
+			var split_value = value.split('\t');
+			var num_tabs = split_value.length - 1;
+			if(num_tabs > previous_line){
+				html += "<ul>";
+			}
+			html += '<li>' + split_value[split_value.length-1] + '</li>';
+			previous_line = num_tabs;
+		}
+	});
+	callback(html);
 }
+
 fs.readFile(process.argv[2], 'utf8', function(err, data){
 	if(err){
 		console.log(err);
 	} else {
-		parseFile(data);
+		parseFile(data, function(html){
+			console.log(html);
+		});
 	}
 });
