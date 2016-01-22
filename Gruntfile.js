6c9e039e0c9c2cf9ec2154963a95326ce2e27794
Matt Huntington <matt.huntington@gmail.com>
2016-01-22 13:44:23 -0500

    adding grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: {
+          port: 35729
+        }
+      },
+      all: {
+        files: ['**/*'],
+        tasks: []
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+};
