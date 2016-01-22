ccf3da7962176920d7a9bc9d64efca6255c13da0
Matt Huntington <matt.huntington@gmail.com>
2016-01-22 13:54:38 -0500

    using tabs in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,21 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      options: {
-        livereload: {
-          port: 35729
-        }
-      },
-      all: {
-        files: ['**/*'],
-        tasks: []
-      }
-    }
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			options: {
+				livereload: {
+					port: 35729
+				}
+			},
+			all: {
+				files: ['**/*'],
+				tasks: []
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['watch']);
+
 };
