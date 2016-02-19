0528c1a67f3b55be2b1b29798057927098e69b3b
Matt Huntington <matt.huntington@gmail.com>
2016-02-19 08:18:47 -0500

    separate tests for different aspects of parser

--- tests/links-spec.js
+++ tests/links-spec.js
@@ -19,23 +19,3 @@ describe("parser", function () {
     });
   });
 });
-describe("parser", function () {
-  it("should render lists", function (done) {
-    fs.readFile('tests/input/lists.txt', 'utf8',function(err, input){
-      fs.readFile('tests/output/lists.html', 'utf8',function(err, output){
-        expect(converter(input)+'\n').toBe(output);
-        done();
-      });
-    });
-  });
-});
-describe("parser", function (done) {
-  it("should render paragraphs and headers", function (done) {
-    fs.readFile('tests/input/single_lines.txt', 'utf8',function(err, input){
-      fs.readFile('tests/output/single_lines.html', 'utf8',function(err, output){
-        expect(converter(input)+'\n').toBe(output);
-        done();
-      });
-    });
-  });
-});
